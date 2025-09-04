444fa66ceb35b0938c531054927448f84fdd848e
Emilio <andere@uchicago.edu>
2025-09-04 20:36:42 +0000

    compiling on Ampere not Ada

--- scripts/eval_from_generations.py
+++ scripts/eval_from_generations.py
@@ -57,7 +57,7 @@ class EvalConfig(Config):
 
         # Construct this from mapping from architecture name to torch cuda arch list in the future
         # you can either specify SM version or just use the name
-        self.gpu_arch = ["Ada"]
+        self.gpu_arch = ["Ampere"]
 
         # Logging
         # Top Directory to Store Runs
